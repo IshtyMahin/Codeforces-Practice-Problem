ee1d9b8c1ab9b3f3dda95addc652a0939a8dd096
Ishtiaq uddin <ishty119@gmail.com>
2022-10-21 19:47:37 +0600

    gitignore setup-1

--- A_Brain_s_Photos.cpp
+++ A_Brain_s_Photos.cpp
@@ -2,23 +2,31 @@
 using namespace std;
 char c;
 int a, b, color;
-int main() {
+int main()
+{
     cin >> a >> b;
-    for (int i = 0; i < a; i++) {
-        for (int j = 0; j < b; j++) {
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
             cin >> c;
-            if (c == 'C' || c == 'M' || c == 'Y') {
+            if (c == 'C' || c == 'M' || c == 'Y')
+            {
                 color = 1;
                 break;
             }
         }
-        if (color == 1) {
+        if (color == 1)
+        {
             break;
         }
     }
-    if (color == 1) {
+    if (color == 1)
+    {
         cout << "#Color";
-    } else {
+    }
+    else
+    {
         cout << "#Black&White";
     }
 }
